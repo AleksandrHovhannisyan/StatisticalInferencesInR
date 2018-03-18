8cf6ad05e746af18715dfd4f46af5141ac4fadb5
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2018-03-18 15:57:06 -0400

    GetSignificanceLevel.R renamed to GetConfidenceLevel.R

--- R/GetSignificanceLevel.R
+++
@@ -1,5 +0,0 @@
-#' Used internally by all functions using alpha. Generates a string in the form of 100(1-alpha) percent
-#' @keywords internal
-GetConfidenceLevel <- function(alpha){
-  return(cat("\n", 100*(1-alpha),"%", sep=""))
-}
